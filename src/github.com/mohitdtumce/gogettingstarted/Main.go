780894498ff1f7e55bce8bac07b42be88b51e153
mohitsharma-r <mohitsharma.r@flipkart.com>
2020-05-22 12:52:31 +0530

    2.0 Variable Initialization

--- src/github.com/mohitdtumce/gogettingstarted/Main.go
+++ src/github.com/mohitdtumce/gogettingstarted/Main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hello World")
+	// Variable Initialization
+	// Method 1
+	var i int
+	i = 31
+
+	// Method 2
+	var j int = 32
+
+	// Method 3
+	k := 33
+	
+	fmt.Println(i, j, k)
 }
