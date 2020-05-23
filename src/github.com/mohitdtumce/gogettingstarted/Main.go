8e3b2ea2a60a5f4ee4067517543ab08bd9694770
mohitsharma-r <mohitsharma.r@flipkart.com>
2020-05-23 15:18:30 +0530

    4.0 Constants

--- src/github.com/mohitdtumce/gogettingstarted/Main.go
+++ src/github.com/mohitdtumce/gogettingstarted/Main.go
@@ -1,104 +1,112 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-func main()  {
+// Constant declared at package level.
+const random string = "Mohit Sharma"
+
+// Constant block
+const (
+	// iota is like a counter
+	_ = iota + 5
+	alpha
+	beta
+	gamma
+)
+
+// We are using a blank identifier.
+// => Yes we know this is going to return a value but we don't care what value it returns.
+const (
+	_  = iota
+	KB = float64(1 << (10 * iota))
+	MB
+	GB
+	TB
+	PB
+	EB
+	ZB
+)
+
+const (
+	isAdmin = 1 << iota
+	isHeadquarters
+	canSeeFinancial
+
+	canSeeAfrica
+	canSeeAsia
+	canSeeEurope
+	canSeeNorthAmerica
+	canSeeSouthAmerica
+)
+
+func main() {
 	// ==================================================
-	// 3.0 Primitives
-
-	// ==================================================
-	// 3.1 Boolean
-	var w bool
-	var x bool = true
-	var y bool = false
-	z := (1 == 1)
-	fmt.Printf("%v, %T\n", w, w)
-	fmt.Printf("%v, %T\n", x, x)
-	fmt.Printf("%v, %T\n", y, y)
-	fmt.Printf("%v, %T\n", z, z)
-
-
-	// ==================================================
-	// 3.2 Numeric - Integer, Floating, Complex
-	
-	// Signed Integer Type 
-	// int8 	[-128, 127]
-	// int16	[-32 768, 32 767]
-	// int32	[-2 147 483 648, 2 147 483 647]
-	// int64	[-9 223 372 036 854 775 808, 9 223 372 036 854 775 807]
-
-	// Unsigned Integer Type 
-	// uint8 	[0, 255]
-	// uint16	[0, 65 535]
-	// uint32	[0, 4 294 967 295]
-	// We don't have 64 bit unsigned integer
-
-	// Note -> We cannot do operations on two operands of different types unless we do explicit type casting
-	var i int8 = 10
-	var j int = 2
-	k := (int(i) + j)
-	fmt.Printf("%v, %T\n", k, k)
-
-	// Bitwise operators
-	var a int = 10  		// 1010
-	var b int = 3			// 0011
-	fmt.Println(a & b)		// 0010 => 2
-	fmt.Println(a | b)		// 1011	=> 11
-	fmt.Println(a ^ b)		// 1001	=> 9
-
-	var c int = 3 			// 0011
-	fmt.Println(c << 1)		// 0110 => 6
-	fmt.Println(c << 3)		// 11000 => 24
-	fmt.Println(c >> 1)		// 0001 => 1
-	fmt.Println(c >> 2)		// 0000 => 0
-
-	// Floating Type
-	// float32 [+/-1.8E(-38), +/- 3.4E(38)]
-	// float64 [+/-2.23E(-308), +/- 1.8E(308)] default
-
-	var m float32 = 3.14
-	fmt.Printf("%v, %T\n", m, m)
-	m = 13.7e-72
-	fmt.Printf("%v, %T\n", m, m)
-	m = 2.1e+14
-	fmt.Printf("%v, %T\n", m, m)
-	var n float64 = 13.7e-72
-	fmt.Printf("%v, %T\n", n, n)
-	
-	p := 10.2
-	q := 3.7
-	fmt.Println(p + q)
-	fmt.Println(p - q)
-	fmt.Println(p * q)
-	fmt.Println(p / q)
-
-	// Complex Type
-	// complex64 and complex128 (default)
-
-	var r complex64 = 1 + 2i
-	fmt.Printf("%v, %T\n", real(r), real(r))
-	var s = 2 + 2i
-	fmt.Printf("%v, %T\n", s, s)
+	// 4.0 Constants
 
 	// ==================================================
-	// 3.3 Text
+	// 4.1 Naming Convention
+	// All constants are proceeded with the const keyword.
+	// If you want to export the const to the outside world, use PascalCase
+	const Exportable float64 = math.Pi
+	fmt.Printf("%v, %T\n", Exportable, Exportable)
+	// Else you can just use normal camelCase
+	const myConst int = 101
+	fmt.Printf("%v, %T\n", myConst, myConst)
 
-	str := "This is utf8 encoded => uint8."
-	fmt.Printf("%v, %T\n", str, str)
-	// Note -> String can be treated as collection of characters
-	fmt.Printf("%v, %T\n", str[2], str[2])
-	fmt.Printf("%v, %T\n", string(str[2]), str[2])
-	// Note -> Strings are immutable. So str[2] = 'u' won't work.
+	// Needless to say that reassigning value of constant (for ex: myConst = 32) won't work.
+	// Value of constant must be determined at compile time and not runtime.
+	// Therefore (const myAnotherConst float64 = math.sin(1.57)) won't work as well.
+	// This is because value of math.sin(1.57) will be determined at runtime.
 
-	// String concatenation
-	anotherStr := " This is also utf8 encoded."
-	fmt.Printf("%v, %T\n", str + anotherStr, str + anotherStr)
+	// Constants can be made up of any primitive types that we discussed earlier.
+	const a int = 12
+	const b float64 = 3.14
+	const c bool = true
+	const d complex128 = 1 + 1i
+	const e string = "This is also a constant"
+	fmt.Printf("%v, %T\n", a, a)
+	fmt.Printf("%v, %T\n", b, b)
+	fmt.Printf("%v, %T\n", c, c)
+	fmt.Printf("%v, %T\n", d, d)
+	fmt.Printf("%v, %T\n", e, e)
 
-	// Converting string into byte arrays
-	byteArray := []byte(str)
-	fmt.Printf("%v, %T\n", byteArray, byteArray)
+	// Note -> Arrays will always be variable type
+	// Note -> Constants can be shadowed. Notice we declare a constant shadow on package level.
+	// We can redeclare the constant inside the block. Inner block will get precedence.
+	fmt.Printf("%v, %T\n", random, random)
+	const random float64 = 10000.0
+	fmt.Printf("%v, %T\n", random, random)
+	// ==================================================
+	// 4.2 Typed constants and untyped constants
+	// So far we've discussed typed constants where we specify type of the constant in the declaration statement.
+	// But we don't necessarily have to do that. We can use compiler's ability to infer the type for us.
+	const myYetAnotherConst = 3.14
+	fmt.Printf("Untyped constant: %v, %T\n", myYetAnotherConst, myYetAnotherConst)
 
-	// rune represents utf32 int32 character
-	var rn rune = 'a'
-	fmt.Printf("%v, %T\n", rn, rn)
+	const w = 42
+	var x = 12.
+	var y = 1 + 32i
+	fmt.Printf("%v, %T\n", w+x, w+x) // Compiler sees this as 42 + x and interpret the type based on the type of x
+	fmt.Printf("%v, %T\n", w+y, w+y) // Notice the type of w+x and w+y
+	// So compiler did implicit type conversion in case of untyped constants which we cannot do in case of variables
+
+	// ==================================================
+	// 4.3 Enumerated Constants
+	fmt.Printf("%v\n", alpha)
+	fmt.Printf("%v\n", beta)
+	fmt.Printf("%v\n", gamma)
+
+	// ==================================================
+	// 4.4 Enumeration Expression
+	filesize := 4000000000.
+	fmt.Println(KB, MB, GB, TB, PB, EB, ZB)
+	fmt.Printf("%.2fGB\n", filesize/GB)
+
+	var roles byte = isAdmin | canSeeFinancial | canSeeEurope
+	fmt.Printf("%b\n", roles)
+
+	fmt.Printf("Is Admin? %v\n", isAdmin&roles == isAdmin)
 }
