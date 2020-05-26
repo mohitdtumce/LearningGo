39141cec208cd60d0429ff1e9cab67d69ae365cb
mohitsharma-r <mohitsharma.r@flipkart.com>
2020-05-26 23:36:24 +0530

    9.1 Defer Statement

--- src/github.com/mohitdtumce/gogettingstarted/Main.go
+++ src/github.com/mohitdtumce/gogettingstarted/Main.go
@@ -2,97 +2,56 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 func main() {
 	// ==================================================
-	// 8.0 Looping
+	// 9.0 Defer, Panic and Recovery
 
-	// Simple for loop (initializer, test, incrementer)
-	for i := 0; i < 5; i += 2 {
-		fmt.Println(i)
-	}
+	/*
+	A defer statement defers the execution of a function until the surrounding function returns
+	*/
+	fmt.Println("Start")
+	defer fmt.Println("Middle-I")
+	defer fmt.Println("Middle-II")
+	fmt.Println("End")
+	/*
+	Deferred functions execute in LIFO order (Stack). So the output of the above snippet of code will be :-
+	Start
+	End
+	Middle-II
+	Middle-I
+	*/
 
-	// More than one iterators (initializer, test, incrementer)
-	for i, j := 1, 0; i < 20; i, j = i*2, j+1 {
-		fmt.Println(i, j)
+	// Example - 2
+	// Open the resource
+	res, err := http.Get("https://www.google.com/robots.txt")
+	if err != nil {
+		log.Fatal(err)
 	}
+	// Close the resource
+	defer res.Body.Close()
+	/* 
+		If you are working with a lot of resources then you should not use the defer statement. 
+		This is because all the opened resources will be closed only once the application executes 
+		and in the meantime those opened resouces will clog the memory
+	*/
 
-	// Now iterator is scoped to main method instead of just the for-loop
-	// Iterator and Increment expressions inside the for-loop body
-	itr := 0 
-	for itr < 5 {
-		fmt.Println("Another form", itr)
-		itr++
+	robots, err := ioutil.ReadAll(res.Body)
+	
+	if err != nil {
+		log.Fatal(err)
 	}
+	fmt.Printf("%s", robots)
 
-	// [Early Exit] break keyword
-	jtr := 1
-	for {
-		fmt.Print(jtr, " ")
-		if jtr == 10 {
-			fmt.Println()
-			break
-		}
-		jtr++
-	}
+	// The deferred call's arguments are evaluated immediately, but the function call is not executed until the surrounding function returns.
+	// So, the output of the below code snipet will be "start" and not "end"
+	a := "start"
+	defer fmt.Println(a)
+	a = "end"
 
-	// [Early Exit] continue keywork
-	for i := 1; i < 4; i++ {
-		if i%2 == 0 {
-			continue
-		}
-		fmt.Println(i)
-	}
 
-	// Nested for-loops
-	for i := 1; i <= 4; i++ {
-		for j := 1; j <= i; j++ {
-			fmt.Print("*")
-		}
-		fmt.Println()
-	}
-
-	// [Early Exit] Labels: To break from all the loops under the label 'MyLabel'
-	MyLabel:
-	for i := 1; i <= 5; i++ {
-		for j := 1; j <= 5; j++ {
-			if i*j >= 8 {
-				fmt.Println()
-				break MyLabel
-			}
-			fmt.Print(i * j)
-		}
-		fmt.Println()
-	}
-
-	// Looping over collections
-	// Iterating over a collection using for-range
-	s := []int{11, 12, 13}
-	for k, v := range s {
-		fmt.Println(k, v)
-	}
-
-	covidCases := map[string]float32{
-		"USA":    1.708,
-		"Brazil": 0.376,
-		"Russia": 0.362,
-	}
-
-	for k, v := range covidCases {
-		fmt.Println(k, v)
-	}
-
-	for k := range covidCases {
-		fmt.Print(k, )
-	}
-
-	for _, v := range covidCases {
-		fmt.Print(v, )
-	}
-
-	str := "Hello World!!"
-	for k, v := range str {
-		fmt.Println(k, string(v))
-	}
 }
