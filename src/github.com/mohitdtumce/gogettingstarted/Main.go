fb1cf79f50515e5e68f0154aedb7ca1f14d8abac
mohitsharma-r <mohitsharma.r@flipkart.com>
2020-05-28 00:48:31 +0530

    12.0 Interfaces (Incomplete)

--- src/github.com/mohitdtumce/gogettingstarted/Main.go
+++ src/github.com/mohitdtumce/gogettingstarted/Main.go
@@ -1,158 +1,172 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 )
 
-func sayMessage(msg, name *string) {
-	fmt.Println(*msg, *name)
-	*name = "Ted"
-	fmt.Println(*name)
+// ==================================================
+// Example 1
+
+// Writer Interface
+// Interfaces doesn't describe data rather behaviour so there will not be any data fields present. Instead there will be method signatures.
+type Writer interface {
+	Write([]byte) (int, error)
 }
 
-func sum(msg string, values ...int) {
-	fmt.Println(values)
-	result := 0
-	for _, v := range values {
-		result += v
+// ConsoleWriter will implement Writer
+// Implicit implementation of interface can be done using structs
+type ConsoleWriter struct {
+}
+
+func (cw ConsoleWriter) Write(data []byte) (int, error) {
+	n, err := fmt.Println(string(data))
+	return n, err
+}
+
+// ==================================================
+// Example 2
+
+// Incrementer Interface
+type Incrementer interface {
+	Increment() int
+}
+
+// IntCounter of type int
+type IntCounter int
+
+// Increment method implemented
+func (ic *IntCounter) Increment() int {
+	*ic++
+	return int(*ic)
+}
+
+// ==================================================
+// Example 3: Composing Interfaces
+
+// Writr Interface
+type Writr interface {
+	Write([]byte) (int, error)
+}
+
+// Closr Interface
+type Closr interface {
+	Close() error
+}
+
+// WritrClosr Interface
+type WritrClosr interface {
+	// Embedding interfaces into one another
+	Writr
+	Closr
+}
+
+// BufferedWritrClosr Struct
+type BufferedWritrClosr struct {
+	// A Buffer is a variable-sized buffer of bytes with Read and Write methods.
+	// A Buffer needs no initialization
+	buffer *bytes.Buffer
+}
+
+// Write Method Implementation
+func (bwc *BufferedWritrClosr) Write(data []byte) (int, error) {
+	/*
+		func (b *Buffer) Write(p []byte) (n int, err error)
+
+		Write appends the contents of p to the buffer, growing the buffer as needed. The return value n is the length of p; err is always nil.
+		If the buffer becomes too large, Write will panic with ErrTooLarge.
+	*/
+	n, err := (*bwc).buffer.Write(data)
+	if err != nil {
+		return 0, err
 	}
-	fmt.Println(msg, result)
-}
 
-func summation(values ...int) int {
-	fmt.Println("Summation returning result :-")
-	fmt.Println(values)
-	result := 0
-	for _, v := range values {
-		result += v
+	v := make([]byte, 8)
+	/*
+		func (b *Buffer) Len() int
+
+		Len returns the number of bytes of the unread portion of the buffer;
+		b.Len() == len(b.Bytes()).
+	*/
+	for (*bwc).buffer.Len() > 8 {
+		/*
+			func (b *Buffer) Read(p []byte) (n int, err error)
+
+			Read: reads the next len(p) bytes from the buffer or until the buffer is drained. The return value n is the number of bytes read.
+			If the buffer has no data to return, err is io.EOF (unless len(p) is zero); otherwise it is nil.
+		*/
+		_, err := (*bwc).buffer.Read(v)
+		if err != nil {
+			return 0, err
+		}
+
+		_, err = fmt.Println(string(v))
+		if err != nil {
+			return 0, err
+		}
 	}
-	return result
+	return n, nil
 }
 
-func summationPtr(values ...int) *int {
-	fmt.Println("Summation returning pointer to local variable :-")
-	fmt.Println(values)
-	result := 0
-	for _, v := range values {
-		result += v
+// Close Method Implementation
+func (bwc *BufferedWritrClosr) Close() error {
+	fmt.Println("Length of buffer", (*bwc).buffer.Len())
+	for (*bwc).buffer.Len() > 0 {
+		/*
+			func (b *Buffer) Next(n int) []byte
+
+			Next returns a slice containing the next n bytes from the buffer, advancing the buffer as if the bytes had been returned by Read.
+			If there are fewer than n bytes in the buffer, Next returns the entire buffer. The slice is only valid until the next call to a read or write method.
+		*/
+		data := (*bwc).buffer.Next(8)
+		_, err := fmt.Println(string(data))
+		if err != nil {
+			return err
+		}
 	}
-	return &result
+	return nil
 }
 
-func summationNamed(values ...int) (result int) {
-	// Named returns
-	fmt.Println(values)
-	for _, v := range values {
-		result += v
+// NewBufferedWritrClosr Method 
+func NewBufferedWritrClosr() *BufferedWritrClosr {
+	return &BufferedWritrClosr{
+		buffer: bytes.NewBuffer([]byte{}),
 	}
-	// Naked return
-	return
+	
 }
-
-func division(a, b float64) (float64, error) {
-	fmt.Println("Multiple returns")
-	if b == 0.0 {
-		return 0.0, fmt.Errorf("Cannot divide by zero")
-	}
-	return a / b, nil
-}
-
-// Method for a struct
-type greeter struct {
-	greeting string
-	name string
-}
-
-func (g greeter) sayHello() {
-	g.name = "Rohit"
-	fmt.Println(g.greeting + " " + g.name)
-}
-
-func (g *greeter) sayHi(){
-	(*g).name = "Rohit"
-	fmt.Println((*g).greeting + " " + (*g).name)
-}
-
-// /*
-// Go does not support function overloading and does not support user defined operators.
-// While Go still does not have overloaded functions (and probably never will), the most useful feature of overloading,
-// that of calling a function with optional arguments and inferring defaults for those omitted can be simulated using
-// a variadic function, which has since been added. But this comes at the loss of type checking
-// */
-// func sayMessage(msg, name string) {
-// }
 
 func main() {
 	// ==================================================
-	// 11.0 Functions -
+	// 12.0 Interface -
 	/*
-		1. Basic Syntax,
-		2. Parameters,
-		3. Return values,
-		4. Anonymous functions
-		5. Functions as types
-		6. Methods
+		1. Basics,
+		2. Composing Interfaces,
+		3. Type Conversion,
+			3.1 The Empty Interface
+			3.2 Type Switches
+		4. Implementing with value vs interface
+		5. Best Practices
 	*/
 
-	greetings := "Hello"
-	name := "Stacey"
-	// Apart from slices and maps which use internal pointers, variables are passed by values.
-	// So if you want to make changes in the original copy, make sure to pass variables
-	sayMessage(&greetings, &name)
-	fmt.Println(name)
+	// Example 1: Inplementation of interface using struct
+	var w Writer = ConsoleWriter{}
+	w.Write([]byte("Hello World"))
 
-	// Variadic Parameters
-	sum("The sum is:", 1, 2, 3, 4, 5)
-	fmt.Println(summation(1, 2, 3, 4, 5))
-
-	resultPtr := summationPtr(1, 2, 3, 4, 5)
-	fmt.Println(*resultPtr)
-
-	result := summationNamed(1, 2, 3, 4, 5)
-	fmt.Println(result)
-
-	quo, err := division(12.0, 0.0)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(quo)
+	// Example 2:
+	var myInt IntCounter = 0
+	var inc Incrementer = &myInt
+	for i := 0; i < 10; i++ {
+		fmt.Println(inc.Increment())
 	}
+	fmt.Println(inc)
 
-	// Anonymous function :- Generally used to generate isolated scope
-	func ()  {
-		fmt.Println("Anonymous Function")
-	}()
+	// Example 3: Composing Interfaces
+	var wc WritrClosr = NewBufferedWritrClosr()
+	wc.Write([]byte("I Learnt About Buffer Package. Nice Hurray !!"))
+	wc.Close()
 
-	// Passing arguments in anomymous function
-	func (name string, age int)  {
-		fmt.Println(name, age)
-	}("Mohit", 24)
+	// Type Conversion interface.(type)
+	bwc := wc.(*BufferedWritrClosr)
+	fmt.Println(bwc)
 
-	// Functions as types
-	var f func() = func ()  {
-		fmt.Println("Yo Bro!!")
-	}
-
-	f()
-
-	divide := func(a, b float64) (float64, error){
-		fmt.Println("Multiple returns")
-		if b == 0.0 {
-			return 0.0, fmt.Errorf("Cannot divide by zero")
-		}
-		return a / b, nil
-	}
-
-	d, e := divide(12.0, 5.0)
-	if e != nil {
-		fmt.Println(e)
-	} else {
-		fmt.Println(d)
-	}
-
-	g := greeter { greeting: "Hello", name: "Mohit",}
-	g.sayHello()
-	fmt.Println(g.name)
-	(&g).sayHi()
-	fmt.Println(g.name)
 }
