4d460422d76c8c85c80f91b62a9a2324f9548d3f
mohitsharma-r <mohitsharma.r@flipkart.com>
2020-05-28 02:18:43 +0530

    13.0 Goroutines

--- src/github.com/mohitdtumce/gogettingstarted/Main.go
+++ src/github.com/mohitdtumce/gogettingstarted/Main.go
@@ -1,172 +1,164 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"runtime"
+	"sync"
+	"time"
 )
 
 // ==================================================
 // Example 1
-
-// Writer Interface
-// Interfaces doesn't describe data rather behaviour so there will not be any data fields present. Instead there will be method signatures.
-type Writer interface {
-	Write([]byte) (int, error)
-}
-
-// ConsoleWriter will implement Writer
-// Implicit implementation of interface can be done using structs
-type ConsoleWriter struct {
-}
-
-func (cw ConsoleWriter) Write(data []byte) (int, error) {
-	n, err := fmt.Println(string(data))
-	return n, err
+func sayHello() {
+	fmt.Println("Hello")
 }
 
 // ==================================================
-// Example 2
+// Example 3 : Using WaitGroups
+var wg = sync.WaitGroup{}
 
-// Incrementer Interface
-type Incrementer interface {
-	Increment() int
+var counter = 0
+
+func greet() {
+	fmt.Printf("Hola Mi Amigo #%v\n", counter)
+	wg.Done()
 }
 
-// IntCounter of type int
-type IntCounter int
-
-// Increment method implemented
-func (ic *IntCounter) Increment() int {
-	*ic++
-	return int(*ic)
+func increment() {
+	counter++
+	wg.Done()
 }
 
 // ==================================================
-// Example 3: Composing Interfaces
+// Example 4 : Using Mutex
+var wgroup = sync.WaitGroup{}
+var ctr = 0
 
-// Writr Interface
-type Writr interface {
-	Write([]byte) (int, error)
+/* 
+	A RWMutex is a reader/writer mutual exclusion lock. 
+	The lock can be held by an arbitrary number of readers or a single writer. 
+	The zero value for a RWMutex is an unlocked mutex.
+*/
+var m = sync.RWMutex{}
+
+func sayHi() {
+	fmt.Printf("Hola Mi Amigo #%v\n", ctr)
+	m.RUnlock()
+	wgroup.Done()
 }
 
-// Closr Interface
-type Closr interface {
-	Close() error
-}
-
-// WritrClosr Interface
-type WritrClosr interface {
-	// Embedding interfaces into one another
-	Writr
-	Closr
-}
-
-// BufferedWritrClosr Struct
-type BufferedWritrClosr struct {
-	// A Buffer is a variable-sized buffer of bytes with Read and Write methods.
-	// A Buffer needs no initialization
-	buffer *bytes.Buffer
-}
-
-// Write Method Implementation
-func (bwc *BufferedWritrClosr) Write(data []byte) (int, error) {
-	/*
-		func (b *Buffer) Write(p []byte) (n int, err error)
-
-		Write appends the contents of p to the buffer, growing the buffer as needed. The return value n is the length of p; err is always nil.
-		If the buffer becomes too large, Write will panic with ErrTooLarge.
-	*/
-	n, err := (*bwc).buffer.Write(data)
-	if err != nil {
-		return 0, err
-	}
-
-	v := make([]byte, 8)
-	/*
-		func (b *Buffer) Len() int
-
-		Len returns the number of bytes of the unread portion of the buffer;
-		b.Len() == len(b.Bytes()).
-	*/
-	for (*bwc).buffer.Len() > 8 {
-		/*
-			func (b *Buffer) Read(p []byte) (n int, err error)
-
-			Read: reads the next len(p) bytes from the buffer or until the buffer is drained. The return value n is the number of bytes read.
-			If the buffer has no data to return, err is io.EOF (unless len(p) is zero); otherwise it is nil.
-		*/
-		_, err := (*bwc).buffer.Read(v)
-		if err != nil {
-			return 0, err
-		}
-
-		_, err = fmt.Println(string(v))
-		if err != nil {
-			return 0, err
-		}
-	}
-	return n, nil
-}
-
-// Close Method Implementation
-func (bwc *BufferedWritrClosr) Close() error {
-	fmt.Println("Length of buffer", (*bwc).buffer.Len())
-	for (*bwc).buffer.Len() > 0 {
-		/*
-			func (b *Buffer) Next(n int) []byte
-
-			Next returns a slice containing the next n bytes from the buffer, advancing the buffer as if the bytes had been returned by Read.
-			If there are fewer than n bytes in the buffer, Next returns the entire buffer. The slice is only valid until the next call to a read or write method.
-		*/
-		data := (*bwc).buffer.Next(8)
-		_, err := fmt.Println(string(data))
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-// NewBufferedWritrClosr Method 
-func NewBufferedWritrClosr() *BufferedWritrClosr {
-	return &BufferedWritrClosr{
-		buffer: bytes.NewBuffer([]byte{}),
-	}
-	
+func increase() {
+	ctr++
+	fmt.Println("Increasing ctr", ctr)
+	m.Unlock()
+	wgroup.Done()
 }
 
 func main() {
 	// ==================================================
-	// 12.0 Interface -
+	// 13.0 Goroutines -
 	/*
-		1. Basics,
-		2. Composing Interfaces,
-		3. Type Conversion,
-			3.1 The Empty Interface
-			3.2 Type Switches
-		4. Implementing with value vs interface
-		5. Best Practices
+		1. Create Goroutines,
+		2. Synchronization,
+			a. WaitGroups
+			b. Mutexes
+		3. Parallelism
+		4. Best Practices & Tools
 	*/
 
-	// Example 1: Inplementation of interface using struct
-	var w Writer = ConsoleWriter{}
-	w.Write([]byte("Hello World"))
+	// Example 1: Simple Example of Goroutine
+	go sayHello()
+	time.Sleep(100 * time.Millisecond)
 
-	// Example 2:
-	var myInt IntCounter = 0
-	var inc Incrementer = &myInt
+	// Example 2: Using Anonymous function
+	/*
+		Notice that we have created a dependency between main function and the goroutine.
+		Our anonymous function here is dependent on main function for the variable msg. This can cause problem.
+		Output for the below code snippet is "Goodnight". This is because in most of the cases compiler will not interrupt main() function unless it executes Sleep() method.
+		It essentially means that even though it spawns another goroutine inside the main() function, it will not provide any CPU to newly spawned goroutine and it is still executing main() function.
+		So instead of printing "Hello Again", it is printing "Goodnight". This is called race-condition and something which we want to avoid.
+	*/
+	var msg = "Hello Again"
+	go func() {
+		fmt.Println(msg)
+	}()
+	msg = "Goodnight"
+	time.Sleep(100 * time.Millisecond)
+
+	/*
+		To solve the above mentioned problem, use parameters.
+		Since string is passed by value, we have actually decoupled anotherMessage variable in the main() from the msg variable of the anonymous function.
+	*/
+	var anotherMessage = "Hello Yet Again"
+	go func (msg string)  {
+		fmt.Println(msg)
+	}(anotherMessage)
+
+	anotherMessage = "Goodnight Again"
+	time.Sleep(100 * time.Millisecond)
+
+	/*
+		The solve mentioned above is not the best practices because we are using the Sleep call and thus binding/degrading the performance of the application.
+		Alternative for that is to use WaitGroup. The task of WaitGroup is to synchronize multiple goroutines together.
+
+		For ex: In this case we want to synchronize, we want to
+	*/
+
+	// Example 3: Using WaitGroups
+	var message = "Hello"
+	wg.Add(1)
+	go func (message string)  {
+		fmt.Println(message)
+		wg.Done()
+	}(message)
+	message = "Adiós"
+	wg.Wait()
+
+	/*
+		In the above example, we are synchronizing two goroutines together but only one of the goroutine is really doing the work.
+		main() function is just storing data and spawing other goroutines.
+		But we can have more than one goroutines working on the same data and we might need to synchronize those together.
+	*/
 	for i := 0; i < 10; i++ {
-		fmt.Println(inc.Increment())
+		wg.Add(2)
+		go greet()
+		go increment()
 	}
-	fmt.Println(inc)
+	wg.Wait()
 
-	// Example 3: Composing Interfaces
-	var wc WritrClosr = NewBufferedWritrClosr()
-	wc.Write([]byte("I Learnt About Buffer Package. Nice Hurray !!"))
-	wc.Close()
+	/*
+		In the above example goroutines are racing against each other and there is not synchronization whatsoever.
+		In order to correct this we need to find a way to synchronize these.
+		We are going to introduce something called mutex.
+	*/
 
-	// Type Conversion interface.(type)
-	bwc := wc.(*BufferedWritrClosr)
-	fmt.Println(bwc)
+	/*
+		GOMAXPROCS sets the maximum number of CPUs that can be executing simultaneously and returns the previous setting.
+		If n < 1, it does not change the current setting. The number of logical CPUs on the local machine can be queried with NumCPU.
+		This call will go away when the scheduler improves.
+	*/
 
+	// Example 4: Locking resouces using Mutex
+	runtime.GOMAXPROCS(100)
+	for i := 0; i < 10; i++ {
+		// main function is executing the locks and the spawned goroutines are asynchronously unlocking it once they're done.
+		wgroup.Add(2)
+		m.RLock()
+		go sayHi()
+		m.Lock()
+		go increase()
+	}
+	wgroup.Wait()
+
+	// But above process doesn't perform well because we are constantly locking/unlocking it. Infact it would have worked better without goroutines. However it can be beneficial in cases.
+
+	fmt.Println(runtime.GOMAXPROCS(-1))
+
+	// Best Practices
+	/*
+		1. Don't create goroutines in libraries. Let consumer control concurrency.
+		2. When creating a goroutine, know how it will end. Aboid subtle memory leaks
+		3. Check for race-condition at compile time. try running "go run -race /path/to/main/file/main_file.go"
+		4.  
+	*/
 }
