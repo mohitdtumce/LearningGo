625f42e8dc42790f81d630287562af6e2e1c180c
mohitsharma-r <mohitsharma.r@flipkart.com>
2020-05-27 11:13:23 +0530

    10.0 Pointers

--- src/github.com/mohitdtumce/gogettingstarted/Main.go
+++ src/github.com/mohitdtumce/gogettingstarted/Main.go
@@ -2,75 +2,74 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
 )
 
 func main() {
 	// ==================================================
-	// 9.0 Defer, Panic and Recovery
+	// 10.0 Pointers - 
+	/*
+		1. Creating Pointers, 
+		2. Dereferencing pointers, 
+		3. The new function, 
+		4. Working with nil and 
+		5. Types with internal pointers
+	*/
+	
+	var a int = 42
+	// b is a pointer to integer which is holding memory address of a
+	var b *int = &a 
+	
+	// To get the address of a variable, we use & operator
+	fmt.Println("Memory address where a is stored: ", &a, b)
+	
+	// Dereferencing the pointer will give us the value stored at the memory address which pointer is pointing to.
+	// Type of the pointer will specify how many bytes to read at the pointed memory address.
+	fmt.Println("Value stored at the location is: ", a, *b) 		
+	
+	a = 27
+	fmt.Println("Value stored at the location is: ", a, *b)
+	
+	*b = 36 
+	fmt.Println("Value stored at the location is: ", a, *b)
+	
+	// // Pointer arithmetics :- Go does not support pointer arithmetics to keep it simple.
+	// If however you wish to use pointer arithmetics in the code, you can use "unsafe" package
+	// https://golang.org/pkg/unsafe/
+	p := [3]int32{1, 2,3 }
+	q := &p[0]
+	r := &p[1]
+	fmt.Printf("%v, %p, %p, %p", p, &p, q, r)
 
-	// Classic example of panic situation
-	a, b := 1, 0
-	ans := a / b
-	fmt.Println(ans)
+	// Pointer to user defined object struct
+	var mPtr *myStructs
+	mPtr = &myStructs{foo: 42}
+	fmt.Println(mPtr)
 
-	// Simple Webapplication which listens all the routes specified by "/" and port 8080
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Mushi Mushi"))
-	})
+	// The new function
+	var nPtr *myStructs
+	fmt.Println(nPtr) 		// Will hold the value nil
+	nPtr = new(myStructs)	// We got uninitialized object here
+	(*nPtr).foo = 36		// Dereferencing the pointer to set the value of objects
+	fmt.Println(*nPtr) 
 
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		// For Instance if we try to run this application concurrently in different consoles, we get following error
-		// panic: listen tcp :8080: bind: address already in use
-		panic(err.Error())
+
+	// Primitives, Arrays, String, Object are passed by value unless we are using pointers
+	mohit := myStructs{foo: 42}
+	rohit := mohit
+	rohit.foo = 36
+	fmt.Println(mohit, rohit)
+
+	// Maps, Slices are passed by reference
+	amohit := map[string]string {
+		"foo":"bar",
+		"baz":"buz",
 	}
+	arohit := amohit
+	arohit["foo"] = "car"
+	fmt.Println(amohit, arohit)
 
-	// Example 3: defer executes before panic
-	fmt.Println("Start")
-	defer fmt.Println("This was deferred")
-	panic("Something bad just happened")
-	fmt.Println("End")
-	/*
-		Output of example 3 will be :-
-		Start
-		This was deferred
-		panic: Something bad just happened
-	*/
-
-	// Example 4: Defer and Ananomous functions
-	fmt.Println("Start")
-	defer func ()  {
-		if err := recover(); err != nil {
-			log.Println("Error:", err)
-		}
-	}()
-	panic("Something bad has happened")
-	fmt.Println("End")	// Unreachable code
-
-	// Example 5
-	fmt.Println("Start")
-	panicker()
-	fmt.Println("End")
-	/*
-		Output of example 5 :-
-		Start
-		About the panic
-		2020/05/27 00:02:39 Error: Something bad has happened
-		panic: Something bad has happened [recovered]
-		        panic: Rethrowing panic if error cannot be handled
-	*/
 }
 
-func panicker() {
-	fmt.Println("About the panic")
-	defer func() {
-		if err := recover(); err != nil {
-			log.Println("Error:", err)
-			panic("Rethrowing panic if error cannot be handled")
-		}
-	}()
-	panic("Something bad has happened")
-	fmt.Println("Done Panicking")
+type myStructs struct {
+	foo int
 }
